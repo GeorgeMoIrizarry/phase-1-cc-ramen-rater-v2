a19c9bc195291c854e8087320fcc2c1f5fc08f6d
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-04-10 23:16:20 -0400

    completed core deliverables

--- src/index.js
+++ src/index.js
@@ -1,21 +1,90 @@
-// index.js
+// index.js 
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  let ramenId = ramen.target.id
+  console.log (ramenId)
+  let rating = document.querySelector('#rating-display')
+  const imgPlaceHolder = document.querySelector('#selected-ramen-picture')
+  const namePlaceHolder = document.querySelector('#selected-ramen-name')
+  const restPlaceHolder = document.querySelector('#selected-ramen-restaurant')
+  let comment = document.querySelector('#comment-display')
+  comment.textContent = ''
+  rating.textContent = ''
+  const ramenApi = "http://localhost:3000/ramens";
+  fetch(ramenApi)
+    .then((resp) => resp.json())
+    .then((data) => handleRtCm(data))
+    function handleRtCm(ramenApi) {
+      ramenApi.forEach((ramObj) => {
+        if (ramObj.id == ramenId) {
+          rating.textContent = ramObj.rating
+          comment.textContent = ramObj.comment
+          imgPlaceHolder.src = ramObj.image
+          namePlaceHolder.textContent = ramObj.name
+          restPlaceHolder.textContent = ramObj.restaurant
+        } else return false 
+      })
+    }
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const form = document.querySelector('#new-ramen')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let newJsonObj = {}
+    let inputs = document.querySelectorAll('input')
+    newJsonObj = {
+      name : e.target.name.value,
+      restaurant : e.target.restaurant.value,
+      image : e.target.image.value,
+      rating : e.target.rating.value,
+      comment : e.target.newcomment.value
+    }
+    fetch("http://localhost:3000/ramens", {
+      method : 'POST',
+      headers : {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(newJsonObj)
+    })
+      .then((resp) => resp.json)
+      .then((data) => displayRamens(data))
+    inputs.forEach((input) => {
+      if (input.className == 'clear'){
+        input.value = ''
+      } else return false
+    
+    })
+    
+    
+  })
+  
 }
 
 const displayRamens = () => {
-  // Add code
+  const ramenMenuDiv = document.querySelector('#ramen-menu');
+  const ramenApi = "http://localhost:3000/ramens";
+  fetch(ramenApi)
+    .then((resp) => resp.json())
+    .then((data) => renderRamImg(data))
+  function renderRamImg(ramArr) {
+    return ramArr.forEach((ramObj) => {
+      let img = document.createElement('img')
+      img.src = ramObj.image
+      img.id = ramObj.id
+      ramenMenuDiv.appendChild(img)
+      img.addEventListener('click', handleClick)
+    })
+    
+  }
+  
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  displayRamens()
+  addSubmitListener()
 }
 
 main()
